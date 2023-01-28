bf4268b12a221909a407154aa6631b2378700a4b
jared282 <jared282@gmail.com>
2023-01-27 23:19:39 -0700

    Working on click events

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 'use strict';
 // .textContent is used to manipulate text from HTML fields
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number!';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
-
 // .value field is used to manipulate input fields that have some sort of value
-document.querySelector('.guess').value = 23;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number value defined!';
+  }
+});
